85acd5109fd3afa6bed168bc407d0af2c8f631b8
Lyam <oxlyam@gmail.com>
2025-06-24 15:14:31 -0400

    Resolve merge conflict in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,4 @@
-<<<<<<< HEAD
-
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-=======
-
+// src/main.tsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -19,6 +7,5 @@ import App from './App'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
->>>>>>> 0586fc0ddfcb662ea18ceb0a567de8e4d6b73122
+  </StrictMode>
 )
